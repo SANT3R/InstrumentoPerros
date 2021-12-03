7157a858d870ed29a093d99420abb51806184738
SANT3R <santer28gamer@gmail.com>
2021-12-03 15:04:37 -0500

    Solo RickAndMorty

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-const URL = "https://dog.ceo/api/breeds/image/random";
+const URL = "https://rickandmortyapi.com/api/character";
 const main_card = document.querySelector('#main-card');
 const template_card = document.querySelector('#template-card').content;
 const fragment = document.createDocumentFragment();
-const btn_1 = document.getElementById('btn_1');
-const btn_3 = document.getElementById('btn_3');
-const btn_9 = document.getElementById('btn_9');
-const btn_12 = document.getElementById('btn_12');
+const select = document.getElementById('select');
+let listaApi=[];
 
-btn_1.addEventListener('click', FechtApi);
-btn_3.addEventListener('click', FechtApi3);
-btn_9.addEventListener('click', FechtApi9);
-btn_12.addEventListener('click', FechtApi12);
+FechtNombres();
 
-
-function CreateCard1(perro, tin) {
-    console.log(tin);
-    for (let i = 0; i < numero; i++) {
+function CreateCard(RickAndMorty) {
+    for (let i = 0; i < RickAndMorty.results.length; i++) {
         let clone_template = document.importNode(template_card, true);
-        clone_template.querySelector(".img-card").setAttribute("src", perro.message);
+        clone_template.querySelector(".img-card").setAttribute("src", RickAndMorty.results[i].image);
+        clone_template.querySelector(".img-card").setAttribute('alt', RickAndMorty.results[i].name);
+        clone_template.querySelector(".nombre-card").textContent= `${RickAndMorty.results[i].name}`;
+        clone_template.querySelector(".genero-card").textContent = `Genero: ${RickAndMorty.results[i].gender}`;
         fragment.appendChild(clone_template);
         main_card.appendChild(fragment);
     }
+        
 }
 
 function FechtApi() {
@@ -32,31 +29,61 @@ function FechtApi() {
     })
 }
 
-function FechtApi3() {
-    let numero=3;
-    fetch(URL)
-    .then(response => response.json())
-    .then(card => {
-        main_card.innerHTML = '';
-        CreateCard(card)
-    })
+function CrearOpciones(RickAndMorty){
+    let listaNombres=['Mostrar Todo'];
+    for (let i = 0; i < RickAndMorty.results.length; i++) {
+        listaNombres.push(RickAndMorty.results[i].name);
+        const opciones= document.createElement('option');
+        opciones.setAttribute('id', `opcion${i}`);
+        opciones.setAttribute('value', listaNombres[i]);
+        opciones.textContent = listaNombres[i];
+        fragment.appendChild(opciones);
+        select.appendChild(fragment);
+    }
+    listaApi.push(RickAndMorty);
+    getSelectValue();
 }
-function FechtApi9() {
-    let numero=9;
+
+function FechtNombres() {
     fetch(URL)
     .then(response => response.json())
-    .then(card => {
+    .then(nombre => {
         main_card.innerHTML = '';
-        CreateCard(card)
+        CrearOpciones(nombre)
     })
 }
 
-function FechtApi12() {
-    let numero=12;
-    fetch(URL)
-    .then(response => response.json())
-    .then(card => {
-        main_card.innerHTML = '';
-        CreateCard(card)
-    })
+function getSelectValue(){
+    let selectValue = document.getElementById("select").value;
+
+    for (let i = 0; i < listaApi[0].results.length; i++) {
+        if("Mostrar Todo"==selectValue){
+            FechtApi();
+        }
+        else if(listaApi[0].results[i].name==selectValue){
+            main_card.innerHTML = '';
+            console.log(selectValue);
+            let clone_template = document.importNode(template_card, true);
+            clone_template.querySelector(".img-card").setAttribute("src", listaApi[0].results[i].image);
+            clone_template.querySelector(".img-card").setAttribute('alt', listaApi[0].results[i].name);
+            clone_template.querySelector(".nombre-card").textContent= `${listaApi[0].results[i].name}`;
+            clone_template.querySelector(".genero-card").textContent = `Genero: ${listaApi[0].results[i].gender}`;
+            fragment.appendChild(clone_template);
+            main_card.appendChild(fragment);
+        }
+    }
+
 }
+
+// function Create(RickAndMorty, numero) {
+//     for (let i = 0; i < RickAndMorty.results.length; i++) {
+//         let clone_template = document.importNode(template_card, true);
+//         clone_template.querySelector(".img-card").setAttribute("src", RickAndMorty.results[numero].image);
+//         clone_template.querySelector(".img-card").setAttribute('alt', RickAndMorty.results[numero].name);
+//         clone_template.querySelector(".nombre-card").textContent= `${RickAndMorty.results[numero].name}`;
+//         clone_template.querySelector(".genero-card").textContent = `Genero: ${RickAndMorty.results[numero].gender}`;
+//         fragment.appendChild(clone_template);
+//         main_card.appendChild(fragment);
+//     }
+        
+// }
